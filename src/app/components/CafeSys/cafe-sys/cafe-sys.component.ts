c082d2ff922b073d53492a52f771d0e7af66a5f4
DESKTOP-1777P77\hoath <hoathong99@gmail.com>
2025-02-16 02:02:17 +0700

    commit

--- src/app/components/CafeSys/cafe-sys/cafe-sys.component.ts
+++ src/app/components/CafeSys/cafe-sys/cafe-sys.component.ts
@@ -25,11 +25,57 @@ import { DividerModule } from 'primeng/divider';
   styleUrl: './cafe-sys.component.css',
 })
 export class CafeSysComponent {
-  searchKeyword : string = "";
-  tabOption: string="";
-  setTabOption(num :number){
+  searchKeyword: string = '';
+  tabOption: string = '';
+  setTabOption(num: number) {
     this.tabOption = num.toString();
-    console.log("Change tab to"+this.tabOption);
+    console.log('Change tab to' + this.tabOption);
   }
+}
 
+export class MenuItem {
+  name: string = '';
+  price: number = 0;
+  icon: string = '';
+  count: number = 0;
+  color: string = '';
+  constructor(
+    name?: string,
+    price?: number,
+    icon?: string,
+    count?: number,
+    color?: string
+  ) {}
+  addCount(n: number) {
+    this.count += n;
+  }
+}
+
+export class Table {
+  id: string = '';
+  name: string = '';
+  area: string = '';
+  icon: string = '';
+  bill: Bill = new Bill();
+  color: string = '';
+  constructor() {}
+}
+
+export class Bill {
+  id: string = '';
+  listArray: ItemServing[] = [];
+  subTotal: number = 0;
+  total: number = 0;
+  VAT: number = 0;
+  status: number = 0;
+  method: number = 0;
+  constructor(id?: string, list?: ItemServing[]) {}
+}
+
+export class ItemServing {
+  name: string = '';
+  price: number = 0;
+  id: string = '';
+  image: string = '';
+  constructor(id?: string, name?: string, price?: number, image?: string) {}
 }

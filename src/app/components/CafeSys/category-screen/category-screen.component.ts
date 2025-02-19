570d6d4b8862771fd3649bcb54d36e51db1b1e94
DESKTOP-1777P77\hoath <hoathong99@gmail.com>
2025-02-20 06:53:44 +0700

    Finishing Basic order action flow

--- src/app/components/CafeSys/category-screen/category-screen.component.ts
+++ src/app/components/CafeSys/category-screen/category-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
@@ -16,9 +16,25 @@ import { Category } from '../Dto/Dtos';
   ],
   templateUrl: './category-screen.component.html',
   styleUrl: './category-screen.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class CategoryScreenComponent {
   searchKeyword: string = '';
   @Output() selectedCategory = new EventEmitter<string>();
   
+
+  //dummy--------//------------------------------------------------------------------------------
+  categories: Category[] = [
+    new Category('C001', 'Fast Food', 'fast_food.png', 0, 'red'),
+    new Category('C002', 'Italian', 'italian.png', 0, 'green'),
+    new Category('C003', 'Healthy', 'healthy.png', 0, 'blue'),
+    new Category('C004', 'Grill', 'grill.png', 0, 'orange'),
+    new Category('C005', 'Beverages', 'beverages.png', 0, 'purple')
+  ];
+  //----------------------------------------------------------------------------------------------
+
+  clickOnCategory(cate: string){
+    this.selectedCategory.emit(cate);
+    console.log("emite category "+cate);
+  }
 }
